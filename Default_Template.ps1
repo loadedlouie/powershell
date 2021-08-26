372cd6a5228897d5a85fd6f531d82bbaa0f87e70
loadedlouie <luisrodrigues27@live.com>
2021-08-26 12:52:29 +0100

    Colocar o Nome do Workbook

--- Default_Template.ps1
+++ Default_Template.ps1
@@ -1,1 +1,50 @@
-﻿dir
+workflow NomeDoWorkFlow
+{
+
+param ( 
+    [Parameter (Mandatory = $false)]
+    [object]$WebhookData
+)
+
+$RequestBody = (ConvertFrom-JSON $WebhookData.RequestBody)
+#$Link = $RequestBody.data.LinkToSearchResults
+
+#Alertas CustomLog Searches
+$AlertT1 = $RequestBody.data.essentials.monitoringService
+#Alertas do tipo Metrica
+$AlertT2 = $RequestBody.data.version
+
+
+if ($AlertT1 -eq 'Log Alerts V2'){
+    $AlertName = $RequestBody.data.essentials.alertRule
+    }
+elseif ($AlertT2 -eq '2.0')
+    {
+    $AlertName = $RequestBody.data.context.name
+    }
+else 
+    {
+    $AlertName = $RequestBody.data.AlertRuleName
+    }
+
+
+
+$Description = $RequestBody.data.Description
+
+$control = 0
+
+
+####Entradas Geradas Automaticamente Pela Query Azure Graph ####
+####Entradas Geradas Automaticamente Pela Query Azure Graph ####
+#### ####CAMonitor-SmartPin-AR-AppService-AverageResponseTime########################################################################### 
+
+
+####Entradas Geradas Automaticamente Pela Query Azure Graph ACIMA DESTA LINHA ####
+####Entradas Geradas Automaticamente Pela Query Azure Graph ACIMA DESTA LINHA ####
+#####################################Control Check##############################################################################
+elseif ( $control -ne 1)
+{
+ Write-EventLog –LogName CA.OMS.Applications –Source "CA-SmartPin" –EntryType Error –EventID 9999 –Message "$AlertName"
+}
+
+}
